b21895c1a575cae99af1a9fe986546b39bf1203e
Stanley Nguyen <hung.ngn.the@gmail.com>
2016-11-22 15:43:48 +0800

    page basic layout

---
+++ application.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+  if (this.body.scrollTop > window.innerHeight) {
+    $('.navbar').addClass('navbar-fixed-top');
+    $('#about').css('margin-top', '70px');
+  } else {
+    $('.navbar').removeClass('navbar-fixed-top');
+    $('#about').css('margin-top', '0');
+  }
+
+  $(document).on('scroll', function() {
+    //fix navbar after header
+    if (this.body.scrollTop > window.innerHeight) {
+      $('.navbar').addClass('navbar-fixed-top');
+      $('#about').css('margin-top', '70px');
+    } else {
+      $('.navbar').removeClass('navbar-fixed-top');
+      $('#about').css('margin-top', '0');
+    }
+  });
+
+  $(document).on('click', '.scroll', function(e) {
+    e.preventDefault();
+    smoothScroll($(e.currentTarget).attr('href'));
+  });
+
+});
+
+function smoothScroll(anchor) {
+  $('body').animate({scrollTop: $(anchor).offset().top - 50 }, 'slow');
+}
